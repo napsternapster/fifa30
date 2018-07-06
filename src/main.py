9891dcb0df75d0beca6fd7831dff0bf254374e22
karambaq <0maximq@gmail.com>
2018-07-06 21:40:05 +0300

    add if on coef

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
             except Exception as e:
                 print('Error in fill_data_by_id')
                 continue
-            if g.is_target() and (g.id, g.teams) not in sended_games:
+            if g.is_target() and (g.id, g.teams) not in sended_games and g.target_coef > 1.5:
                 teams = g.teams
                 coef = g.target_coef
                 score = g.score
